d45f589d95c923270cb3bb4de86f3026bc5cb8f7
Anna Thomas <annat@qburst.com>
2020-04-01 10:30:05 +0530

    Tableview : Basic Table view - basic table view added - Constants file added

---
+++ JsonViewer/Utility/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  JsonViewer
+//
+//  Created by AnnaThomas on 01/04/20.
+//  Copyright © 2020 QBurst Technologies. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    // Reuse identifier
+    static let cellReuseIdentifier = "jsonFeederCellIdentifier"
+}
--- JsonViewer/ViewController.swift
+++ JsonViewer/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var jSonFeedertableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // initialize basic table view setup
+        jSonFeedertableView = UITableView(frame: self.view.bounds, style: .plain)
+        jSonFeedertableView.dataSource = self
+        jSonFeedertableView.delegate = self
+        jSonFeedertableView.backgroundColor = .white
+        jSonFeedertableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.cellReuseIdentifier)
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellReuseIdentifier)
+        cell?.textLabel?.text = "This is row \(indexPath.row)"
+        
+        return cell!
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
